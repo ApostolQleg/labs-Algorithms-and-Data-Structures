fd5e1fee9e2ff1e010fa2891e143cd49d2c7e0a4
ApostolOleg <apostololeg0@gmail.com>
2026-04-27 17:51:11 +0300

    Add: implement self-loop drawing functionality in graph visualization

--- sem2/lab4/lab4.c
+++ sem2/lab4/lab4.c
@@ -107,6 +107,52 @@ void draw_arrow(Vector2 start, Vector2 end, float r_node)
     DrawTriangle(v1, v3, v2, GRAY);
 }
 
+void draw_self_loop(Vector2 node, Vector2 center, float r_node)
+{
+    float dx = node.x - center.x;
+    float dy = node.y - center.y;
+
+    float length = sqrtf(dx * dx + dy * dy);
+
+    float dir_x = dx / length;
+    float dir_y = dy / length;
+
+    float offset = r_node * 1.5;
+    float r_loop = r_node * 1.25f;
+
+    Vector2 loop_center = {
+        node.x + dir_x * offset,
+        node.y + dir_y * offset};
+
+    DrawCircleLines(loop_center.x, loop_center.y, r_loop, GRAY);
+
+    float angle_to_node = atan2f(node.y - loop_center.y, node.x - loop_center.x);
+
+    float intersect_angle = 0.7227f;
+
+    float alpha = angle_to_node - intersect_angle;
+
+    float target_x = loop_center.x + r_loop * cosf(alpha);
+    float target_y = loop_center.y + r_loop * sinf(alpha);
+
+    float angle = alpha + (PI / 2.0f);
+
+    float arrow_len = 15.0f;
+    float arrow_angle = PI / 6.0f;
+
+    Vector2 v1 = {target_x, target_y};
+
+    Vector2 v2 = {
+        target_x - arrow_len * cosf(angle - arrow_angle),
+        target_y - arrow_len * sinf(angle - arrow_angle)};
+    Vector2 v3 = {
+        target_x - arrow_len * cosf(angle + arrow_angle),
+        target_y - arrow_len * sinf(angle + arrow_angle)};
+
+    DrawTriangle(v1, v2, v3, GRAY);
+    DrawTriangle(v1, v3, v2, GRAY);
+}
+
 int main()
 {
     int **A_dir = create_matrix(N);
@@ -119,13 +165,14 @@ int main()
     print_matrix(A_dir, N, "Directed Graph Matrix");
     print_matrix(A_undir, N, "Undirected Graph Matrix");
 
+    SetTraceLogLevel(LOG_NONE);
     const int screenWidth = 1600;
     const int screenHeight = 900;
     InitWindow(screenWidth, screenHeight, "Lab 3 - Raylib Graph Representation");
     SetTargetFPS(60);
 
     bool show_directed = true;
-    float R = 400.0f;
+    float R = 300.0f;
     float r_node = 40.0f;
     Vector2 center = {screenWidth / 2.0f, screenHeight / 2.0f};
 
@@ -160,6 +207,10 @@ int main()
                     if (show_directed && i != j)
                     {
                         draw_arrow(nodes[i], nodes[j], r_node);
+                    }
+                    else if (i == j)
+                    {
+                        draw_self_loop(nodes[i], center, r_node);
                     }
                 }
             }
