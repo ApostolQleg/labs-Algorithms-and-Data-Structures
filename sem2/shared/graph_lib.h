06feca514ba4a30d9759eb05b2f8d6a3641eb676
ApostolOleg <apostololeg0@gmail.com>
2026-04-29 12:51:37 +0300

    Refactor: remove unused functions for arrow angle and target calculation in graph library

--- sem2/shared/graph_lib.h
+++ sem2/shared/graph_lib.h
@@ -11,8 +11,6 @@ int mulmr(double value, double k);
 void generate_directed_matrix(int **matrix, int n, double k);
 void generate_undirected_matrix(int **matrix_directed, int **matrix_to_change, int n);
 void print_matrix(int **matrix, int n, const char *title);
-float calc_arrow_angle(Vector2 start, Vector2 end);
-Vector2 calc_arrow_target(Vector2 end, float r_node, float angle);
 void draw_arrow(Vector2 target, float angle);
 void draw_graph(int **matrix, Vector2 *nodes, int n, float r_node, Vector2 center, bool is_directed);
 
